9458787eeb5c8295f5952baef4700f736a75681a
andreareijat <82641203+andreareijat@users.noreply.github.com>
2022-02-24 19:46:28 +0100

    Add files via upload

--- mi_simulador/src/control_turtle1.py
+++ mi_simulador/src/control_turtle1.py
@@ -5,8 +5,11 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from std_srvs.srv import Empty
+from nav_msgs.msg import Odometry
+from collections import deque
 
 def girar_robot(msg):
+
     msg.angular.z=1.6
     rate=rospy.Rate
 
@@ -21,19 +24,27 @@ def mover_robot(msg):
     msg.linear.x=velocidad
     pub.publish(msg)
 
+def atrapado(msg):
 
-def scan_and_move(msg):
+    principio=msg.ranges[0]
+    frente=msg.ranges[len(msg.ranges)/2]
+    final=msg.ranges[len(msg.ranges)-1]
 
+    return (principio<0.25 and frente<0.25 and final<0.25)
+
+def trayectoria(msg):
+
+    global odom
     vel=Twist()
 
-    principio_array=msg.ranges[0:len(msg.ranges)/3]
-    principio=msg.ranges[0]
-    frente_array=msg.ranges[len(msg.ranges):(len(msg.ranges)*2/3)]
-    frente=msg.ranges[len(msg.ranges)/2]
-    final_array=msg.ranges[(len(msg.ranges)*2/3):len(msg.ranges)]
-    final=msg.ranges[len(msg.ranges)]
+    stack=deque()
 
-    direccion_avance=comprueba_dead_end()
+    principio_array=msg.ranges[0:len(msg.ranges)/3]
+    frente_array=msg.ranges[len(msg.ranges):(len(msg.ranges)*2/3)]
+    final_array=msg.ranges[(len(msg.ranges)*2/3):len(msg.ranges)]
+    
+
+    #direccion_avance=comprueba_dead_end()
 
     for i in principio_array:
         if i < 0.5:
@@ -52,18 +63,15 @@ def scan_and_move(msg):
     
     vel.linear.x=0
 
+def odom_callback(msg):
+    global odom
+    odom=msg
 
 
 def laser_callback(msg):
-    global distancia,objetivo
+    global distancia
     distancia = msg.ranges[int(len(msg.ranges)/2)]
-    objetivo=distancia+2
 
-    try:
-        scan_and_move(msg)
-	
-    except rospy.ROSInterruptException:
-        rospy.loginfo("node terminated. ")
 
 #main
 
@@ -71,9 +79,28 @@ rospy.init_node("nodo_control_turtle")
 objetivo=2
 sub=rospy.Subscriber("/base_scan",LaserScan,laser_callback)
 pub=rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+sub=rospy.Subscriber("/odom", Odometry, odom_callback)
 
-distancia=6
 velocidad=0.3
+contador=0
 
+try:
+    trayectoria()
+    contador+=1
+    if contador==1:
+        vector_director_inicial=[posicion_x,posicion_y]
+
+    if contador>1:
+        vector_director_actual=[posicion_x,posicion_y]
+        
+        siguiente=
+        anterior=siguiente
+    direccion=[]
+    direccion.append(vector_director)
+
+
+
+except rospy.ROSInterruptException:
+    rospy.loginfo("node terminated. ")
 
 rospy.spin()
---
+++ mi_simulador/src/deque.py
@@ -0,0 +1,14 @@
+
+
+
+direccion_objetivo
+direccion_actual
+
+if direccion_actual != direccion_objetivo and pila_no_esta_vacia:
+
+    añadir a la pila la direccion
+
+elif direccion actual != direccion objetivo and pila_esta_vacia:
+
+
+
