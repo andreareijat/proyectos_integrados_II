f4e1336451bd8c861326fed86b49544885efa7a0
andreareijat <82641203+andreareijat@users.noreply.github.com>
2022-02-24 16:15:00 +0100

    Add files via upload

--- mi_simulador/src/control_turtle1.py
+++ mi_simulador/src/control_turtle1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from re import I
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
@@ -19,30 +20,47 @@ def mover_robot(msg):
     
     msg.linear.x=velocidad
     pub.publish(msg)
+
+
+def scan_and_move(msg):
+
+    vel=Twist()
+
+    principio_array=msg.ranges[0:len(msg.ranges)/3]
+    principio=msg.ranges[0]
+    frente_array=msg.ranges[len(msg.ranges):(len(msg.ranges)*2/3)]
+    frente=msg.ranges[len(msg.ranges)/2]
+    final_array=msg.ranges[(len(msg.ranges)*2/3):len(msg.ranges)]
+    final=msg.ranges[len(msg.ranges)]
+
+    direccion_avance=comprueba_dead_end()
+
+    for i in principio_array:
+        if i < 0.5:
+            vel.linear.x=0
+            girar_robot(vel)
+    for i in frente_array:
+        if i < 0.25:
+            vel.linear.x=0
+            girar_robot(vel)
+    for i in final_array:
+        if i < 0.5:
+            vel.linear.x=0 
+            girar_robot(vel)
     
-
-def scan_and_move():
-
-    msg=Twist()
-
-    if distancia > 1.0:
-        mover_robot(msg)
-    else:
-        msg.linear.x=0
-        girar_robot(msg)
+    mover_robot(vel)
     
-    msg.linear.x=0
+    vel.linear.x=0
 
 
 
 def laser_callback(msg):
-    global distancia
+    global distancia,objetivo
     distancia = msg.ranges[int(len(msg.ranges)/2)]
+    objetivo=distancia+2
 
-    
-    
     try:
-        scan_and_move()
+        scan_and_move(msg)
 	
     except rospy.ROSInterruptException:
         rospy.loginfo("node terminated. ")
@@ -50,6 +68,7 @@ def laser_callback(msg):
 #main
 
 rospy.init_node("nodo_control_turtle")
+objetivo=2
 sub=rospy.Subscriber("/base_scan",LaserScan,laser_callback)
 pub=rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
