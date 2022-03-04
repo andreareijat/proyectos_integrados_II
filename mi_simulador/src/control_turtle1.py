11b28b6ee643b32fafd1f944f948054118625096
andreareijat <82641203+andreareijat@users.noreply.github.com>
2022-03-04 16:18:29 +0100

    Add files via upload

--- mi_simulador/src/control_turtle1.py
+++ mi_simulador/src/control_turtle1.py
@@ -5,11 +5,8 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from std_srvs.srv import Empty
-from nav_msgs.msg import Odometry
-from collections import deque
 
 def girar_robot(msg):
-
     msg.angular.z=1.6
     rate=rospy.Rate
 
@@ -24,27 +21,19 @@ def mover_robot(msg):
     msg.linear.x=velocidad
     pub.publish(msg)
 
-def atrapado(msg):
 
-    principio=msg.ranges[0]
-    frente=msg.ranges[len(msg.ranges)/2]
-    final=msg.ranges[len(msg.ranges)-1]
+def scan_and_move(msg):
 
-    return (principio<0.25 and frente<0.25 and final<0.25)
-
-def trayectoria(msg):
-
-    global odom
     vel=Twist()
 
-    stack=deque()
-
     principio_array=msg.ranges[0:len(msg.ranges)/3]
+    principio=msg.ranges[0]
     frente_array=msg.ranges[len(msg.ranges):(len(msg.ranges)*2/3)]
+    frente=msg.ranges[len(msg.ranges)/2]
     final_array=msg.ranges[(len(msg.ranges)*2/3):len(msg.ranges)]
-    
+    final=msg.ranges[len(msg.ranges)]
 
-    #direccion_avance=comprueba_dead_end()
+    direccion_avance=comprueba_dead_end()
 
     for i in principio_array:
         if i < 0.5:
@@ -63,44 +52,28 @@ def trayectoria(msg):
     
     vel.linear.x=0
 
-def odom_callback(msg):
-    global odom
-    odom=msg
 
 
 def laser_callback(msg):
-    global distancia
+    global distancia,objetivo
     distancia = msg.ranges[int(len(msg.ranges)/2)]
+    objetivo=distancia+2
 
+    try:
+        scan_and_move(msg)
+	
+    except rospy.ROSInterruptException:
+        rospy.loginfo("node terminated. ")
 
 #main
 
 rospy.init_node("nodo_control_turtle")
 objetivo=2
-sub=rospy.Subscriber("/base_scan",LaserScan,laser_callback)
+sub=rospy.Subscriber("/base_scan_1",LaserScan,laser_callback)
 pub=rospy.Publisher("/cmd_vel", Twist, queue_size=1)
-sub=rospy.Subscriber("/odom", Odometry, odom_callback)
 
+distancia=6
 velocidad=0.3
-contador=0
 
-try:
-    trayectoria()
-    contador+=1
-    if contador==1:
-        vector_director_inicial=[posicion_x,posicion_y]
-
-    if contador>1:
-        vector_director_actual=[posicion_x,posicion_y]
-        
-        siguiente=
-        anterior=siguiente
-    direccion=[]
-    direccion.append(vector_director)
-
-
-
-except rospy.ROSInterruptException:
-    rospy.loginfo("node terminated. ")
 
 rospy.spin()
